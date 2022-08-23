cc52cea8c9edce1922349907baff022aea585a46
david-codez <davidcodesyt@gmail.com>
2022-08-23 14:22:28 -0400

    vercel deployment

--- components/resume/PortfolioSection.jsx
+++ components/resume/PortfolioSection.jsx
@@ -10,19 +10,22 @@ export default function PortfolioSection() {
                 <Row>
                     <Col md='4' align='center'>
                         <Link href='/portfolio/to-do-list'>
-                            <Card border='white' bg ='black' className='portfolio-card' style={{ width: '13rem' }}>
-                                <Card.Img variant='top' src='/images/to-do-list-screenshot.png' alt='to do list' height='200' />
-                                <Card.Body>
-                                    <Card.Title>To Do List App</Card.Title>
-                                    {/* <Card.Text>
-                                        To Do List App
-                                    </Card.Text> */}
-                                </Card.Body>
-                            </Card>
+                            <a>
+                                <Card border='white' bg ='black' className='portfolio-card' style={{ width: '13rem' }}>
+                                    <Card.Img variant='top' src='/images/to-do-list-screenshot.png' alt='to do list' height='200' />
+                                    <Card.Body>
+                                        <Card.Title>To Do List App</Card.Title>
+                                        {/* <Card.Text>
+                                            To Do List App
+                                        </Card.Text> */}
+                                    </Card.Body>
+                                </Card>
+                            </a>
                         </Link>
                     </Col>
                     <Col md='4' align='center'>
                         <Link href='/portfolio/pokedex'>
+                            <a>
                                 <Card border='white' bg ='black' className='portfolio-card' style={{ width: '13rem' }}>
                                     <Card.Img variant='top' src='/images/pokedex-screenshot.png' alt='pokedex' height='200'/>
                                     <Card.Body>
@@ -32,17 +35,20 @@ export default function PortfolioSection() {
                                         </Card.Text> */}
                                     </Card.Body>
                                 </Card>
+                            </a>
                             </Link>
                     </Col>
                     <Col md='4' align='center'>
                         <Link href='/portfolio/hangman' className='portfolio-card'>
-                            <Card border='white' bg ='black' style={{ width: '13rem' }}>
-                                <Card.Img variant='top' src='/images/hangman-screenshot.png' alt='hangman' height='200'/>
-                                <Card.Body>
-                                    <Card.Title>Hangman App</Card.Title>
-                                 
-                                </Card.Body>
-                            </Card>
+                            <a>
+                                <Card border='white' bg ='black' style={{ width: '13rem' }}>
+                                    <Card.Img variant='top' src='/images/hangman-screenshot.png' alt='hangman' height='200'/>
+                                    <Card.Body>
+                                        <Card.Title>Hangman App</Card.Title>
+                                    
+                                    </Card.Body>
+                                </Card>
+                            </a>
                         </Link>
                     </Col>
                 </Row>
